d117745535e85069509a380d7eb144893fb82179
Renan Cunha <renancdev@gmail.com>
2023-05-05 02:23:38 -0300

    Adicionar arquivos de projeto.

---
+++ ProductsAPI/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductsAPI.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public productCategory Category { get; set; }
+    }
+
+    public enum productCategory
+    {
+        Food = 0,
+        Conveniences = 1,
+        Commodities = 2,
+        Durable = 3,
+        Digital = 4
+    }
+}
