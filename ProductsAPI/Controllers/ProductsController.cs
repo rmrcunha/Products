0a74637b2dad0180d8b65fac8991721f0ab821f0
Renan Cunha <renancdev@gmail.com>
2023-05-05 13:26:47 -0300

    lógica de controle criada, assim como a criação do contexto

--- ProductsAPI/Controllers/ProductsController.cs
+++ ProductsAPI/Controllers/ProductsController.cs
@@ -1,12 +1,73 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Data;
+using ProductsAPI.Models;
 
 namespace ProductsAPI.Controllers
 {
-    public class ProductsController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ProductsContext _context;
+
+       public ProductsController(ProductsContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("/products")]
+       public async Task<ActionResult> GetProducts() {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpPost]
+        [Route("/products")]
+        public async Task<ActionResult> CreateProduct(Product product) 
+        {
+            await _context.Products.AddAsync(product);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpPut]
+        [Route("/products")]
+        public async Task<ActionResult> UpdateProduct(Product product)
+        {
+            var dbProduct = await _context.Products.FindAsync(product.Id);
+
+            if(dbProduct == null)
+            {
+                return NotFound();
+            }
+
+            dbProduct.Name = product.Name;
+            dbProduct.Price = product.Price;
+            dbProduct.Category = product.Category;
+
+            await _context.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        [Route("/products")]
+        public async Task<ActionResult> UpdateProduct(Guid id)
+        {
+            var dbProduct = await _context.Products.FindAsync(id);
+
+            if(dbProduct == null )
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(dbProduct);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
--- ProductsAPI/Program.cs
+++ ProductsAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddDbContext<ProductsContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ServerConnection")));
 
 var app = builder.Build();
 
